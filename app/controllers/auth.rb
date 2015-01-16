73500f53f3e2147ebb08454bca94a075cf93cbbc
Amar Singh <ummerr@gmail.com>
2015-01-16 15:27:01 -0500

    added post login, post signup and post signout routes and logic'

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,11 +7,40 @@ get '/login' do
   erb :'user/login'
 end
 
+post '/login' do
+
+  user = User.find_by(name: params[:user][:name])
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/search'
+  else
+    redirect '/login'
+  end
+
+end
+
 get '/signup' do
 
   erb :'user/signup'
 end
 
 post '/signup' do
+
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '/search'
+  else
+    redirect '/signup'
+  end
+
+end
+
+get '/signout' do
+
+  session[:user_id] = nil
+  redirect '/'
 
 end
