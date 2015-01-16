fee7a2c5f6786971bb2f6ec59b2b1aa3e3040dee
sagararathi <bravo.sagar@gmail.com>
2015-01-16 17:42:09 -0500

    updated module for youtube search

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -1,8 +1,8 @@
-class Youtube
+module Youtube
 
     YCLIENT = YouTubeG::Client.new(:dev_key => ENV["YOUTUBE_KEY"])
 
-  def youtube_search string
+  def self.youtube_search string
     result = YCLIENT.videos_by(:query => string)
     result.videos.first.media_content.first.url
   end
