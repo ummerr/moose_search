b6b4e278945b04ac6f48c38d9de1d4dbef3509c6
Daniella Shin & Amar Singh <pair+thedanpan.ummerr@devbootcamp.com>
2015-01-16 12:04:04 -0500

    added empty user and search model and migrations

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,3 @@
-class User < ActiveRecord::Base
+class Search < ActiveRecord::Base
   # Remember to create a migration!
 end
---
+++ db/migrate/20150116120202_create_searches.rb
@@ -0,0 +1,4 @@
+class CreateSearches < ActiveRecord::Migration
+  def change
+  end
+end
