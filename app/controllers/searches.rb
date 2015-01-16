3869133e1229c3c00c53581111571eac687a7087
Amar Singh <ummerr@gmail.com>
2015-01-16 16:03:28 -0500

    adding controller logic - not ready to merge

--- app/controllers/searches.rb
+++ app/controllers/searches.rb
@@ -4,11 +4,23 @@ get '/search' do
 end
 
 get '/search/:id' do
-
+  @search = Search.find(id)
   erb :'search/show'
 end
 
-get '/search/all' do
+post '/search' do
+  new_search = Search.new(params[:search])
+  current_user.searches << new_search
 
-  erb :'search/'
+  redirect '/search/#{new_search.id}'
+end
+
+# get '/search/:name' do
+#   @
+# end
+
+get '/search/all' do
+  @searches = Search.all
+
+  erb :'search/all'
 end
