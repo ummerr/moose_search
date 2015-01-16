de048b3798741237350c98ec44c23784f51df983
Daniella Shin & Amar Singh <pair+thedanpan.ummerr@devbootcamp.com>
2015-01-16 12:07:13 -0500

    added migrations and model relationships for user and search, yet to migrate

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,3 @@
 class Search < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
 end
--- db/migrate/20150116120202_create_searches.rb
+++ db/migrate/20150116120202_create_searches.rb
@@ -1,4 +1,13 @@
 class CreateSearches < ActiveRecord::Migration
   def change
+    create_table :searches do |t|
+      t.string :term
+      t.string :youtube_url
+      t.string :tweet_url
+      t.string :reddit_url
+      t.references :user
+
+      t.timestamps
+    end
   end
 end
