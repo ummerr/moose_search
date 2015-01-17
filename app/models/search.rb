672ff0732329c67ece2ae638ba9c15d2240dc942
Amar Singh <ummerr@gmail.com>
2015-01-17 14:52:52 -0500

    added a date_posted and sort_by methods for search

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,7 +5,15 @@ class Search < ActiveRecord::Base
 
   def get_search_results
     self.youtube = Youtube.youtube_search(self.term)
-    self.reddit_url = Reddit.search(self.term, 1)[:url]
+    # self.reddit_url = Reddit.search(self.term, 1)[:url]
+  end
+
+  def self.all_by_date
+    Search.all.order(created_at: :desc)
+  end
+
+  def date_posted
+    self.created_at.strftime("%D %R %Z")
   end
 
 end
