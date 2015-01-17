3d28030af398bc94c07962de851832a7fa7b201b
Scott Enriquez <sjenriquez@gmail.com>
2015-01-17 16:51:15 -0500

    fix twitter searches to handle unpopular searches

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,7 +5,7 @@ class Search < ActiveRecord::Base
 
   def get_search_results
     self.youtube = Youtube.youtube_search(self.term)
-    self.tweet = Tweet.twitter_search(string)
+    self.tweet = Tweet.twitter_search(self.term)
     # self.reddit_url = Reddit.search(self.term, 1)[:url]
   end
 
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,18 +1,25 @@
 module Tweet
 
-  TCLIENT = Twitter::Streaming::Client.new do |config|
-    config.consumer_key        = ENV["CONSUMER_KEY"]
-    config.consumer_secret     = ENV["CONSUMER_SECRET"]
-    config.access_token        = ENV["ACCESS_TOKEN"]
-    config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+  TCLIENT = Twitter::REST::Client.new do |config|
+    config.consumer_key        = "Dz1vcQeQjtyuYv4Y0s8uSbJR8"
+    config.consumer_secret     = "PsoklQM9enuWcyCh75Q8dLwydPysKgfIlllMUvtbyjQyIG0qzV"
+    config.access_token        = "204986879-nVYxDYg4PlsQnxLIV49s2DzmCkwf9cijlnY7ogHX"
+    config.access_token_secret = "qd1Z6MJceCbi9QKp0TLOS02356u7DTLGmiYqTiuf6VFHq"
   end
 
   def self.twitter_search(string)
-    result = TCLIENT.search(string, result_type: "popular").max_by { |tweet| tweet.retweet_count }
+    # hashed_string = '#' + string
+    result = TCLIENT.search(string, result_type: "popular").first
     if result
-      result.id
+      result.id.to_s
     else
-      nil
+      result = TCLIENT.search(string).max_by(&:retweet_count)
+      if result
+        result.id.to_s
+      else
+        result = TCLIENT.search(string).first
+        result.id.to_s
+      end
     end
   end
 
