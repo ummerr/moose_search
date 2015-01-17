1b9c85f66a26b23de62f6754f97adf77e01ce333
Jay <jhd@fastmail.net>
2015-01-17 17:47:26 -0500

    change before_save, get_search_results to accommodate new Reddit table structure

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,12 +1,14 @@
 class Search < ActiveRecord::Base
   belongs_to :user
+  has_many :RedditSearches
   before_save :get_search_results
-
+  after_save :get_reddit_results
 
   def get_search_results
+
     self.youtube = Youtube.youtube_search(self.term)
     self.tweet = Tweet.twitter_search(self.term)
-    # self.reddit_url = Reddit.search(self.term, 1)[:url]
+    
   end
 
   def self.all_by_date
@@ -15,6 +17,15 @@ class Search < ActiveRecord::Base
 
   def date_posted
     self.created_at.strftime("%D %R %Z")
+  end
+
+  def get_reddit_results
+
+    3.times do |num|
+      reddit_results = RedditSearch.search(self.term, num + 1)
+      RedditSearch.create(search_id: self.id, url: reddit_results[:url], title: reddit_results[:title])
+    end
+
   end
 
 end
