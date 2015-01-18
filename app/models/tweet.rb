e666214c22f0a82a18061d78f7a812538d9b5bfd
Daniella Shin <thedanpan@gmail.com>
2015-01-17 19:30:18 -0500

    add env keys for twitter

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,10 @@
 module Tweet
 
   TCLIENT = Twitter::REST::Client.new do |config|
-    config.consumer_key        =
-    config.consumer_secret     =
-    config.access_token        =
-    config.access_token_secret =
+    config.consumer_key        = ENV["CONSUMER_KEY"]
+    config.consumer_secret     = ENV["CONSUMER_SECRET"]
+    config.access_token        = ENV["ACCESS_TOKEN"]
+    config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
   end
 
   def self.twitter_search(string)
